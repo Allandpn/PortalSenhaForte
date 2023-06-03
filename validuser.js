62a0b7fd70c77656364ebd205e709c3ed71096d0
Allandpn <99135710+Allandpn@users.noreply.github.com>
2023-06-03 15:56:57 -0300

    fixed

--- validuser.js
+++ validuser.js
@@ -11,7 +11,7 @@ const userTemp = {
 
 
 const validUser = () => {
-    const user = JSON.parse(localStorage.getItem("db_user"))
+    const user = JSON.parse(localStorage.getItem("db_user")) ?? []
     const usuario = document.getElementById('input-login-user').value
     const password = document.getElementById("input-login-password").value
 
@@ -24,18 +24,23 @@ const validUser = () => {
         }
     })
 
-    console.log(userTemp)
 
-    if (userTemp.usuario == usuario && userTemp.senha == password) {
-        window.location.href = "home_login.html"          
-        const token = Math.random().toString(16).substring(2)
-        localStorage.setItem('token', token)
 
-    } else {        
-        document.getElementById('input-login-user').setAttribute("style", 'border-color: red')
-        document.getElementById('input-login-password').setAttribute("style", 'border-color: red')
-        document.getElementById('msg-error').setAttribute("style", 'display: block')
-        document.getElementById('input-login-user').focus()
+    if (usuario.length > 0 && password.length > 0) {
+
+        console.log(usuario.length)
+        console.log(password.length)
+        if (userTemp.usuario == usuario && userTemp.senha == password) {
+            window.location.href = "home_login.html"
+            const token = Math.random().toString(16).substring(2)
+            localStorage.setItem('token', token)
+
+        } else {
+            document.getElementById('input-login-user').setAttribute("style", 'border-color: red')
+            document.getElementById('input-login-password').setAttribute("style", 'border-color: red')
+            document.getElementById('msg-error').setAttribute("style", 'display: block')
+            document.getElementById('input-login-user').focus()
+        }
     }
 }
 
